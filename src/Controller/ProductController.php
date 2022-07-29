797e78e06081317b6f2d9db34bdb6237f978c57e
Shkodenko V. Taras <taras.shkodenko@blackbit.de>
2022-07-29 10:37:35 +0300

    Improved /make-product in a ProductController

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,17 +2,40 @@
 
 namespace App\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Service\RandGenerator;
+use App\Entity\Product;
+
 
 class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
-    public function index(): Response
+    #[Route('/make-product', name: 'app_make_product')]
+    public function make(ManagerRegistry $doctrine, RandGenerator $randomGenerator): Response
     {
-        return $this->render('product/index.html.twig', [
+        $entityManager = $doctrine->getManager();
+
+        $randomNumber = $randomGenerator->randomNumber(21, 99);
+
+        $product = new Product();
+        $product->setName('Test Product #' . $randomNumber);
+        $product->setDescription('Test Product #' . $randomNumber . ' description text goes here...');
+        $product->setPrice($randomNumber);
+        $product->setAuthor('Taras');
+        $product->setSku('S019-' . $randomNumber);
+
+        $entityManager->persist($product);
+
+        $entityManager->flush();
+
+        $msg = 'Saved a new product with ID: ' . $product->getId();
+
+        return $this->render('product/make.html.twig', [
             'controller_name' => 'ProductController',
+            'message' => $msg,
         ]);
     }
 }
