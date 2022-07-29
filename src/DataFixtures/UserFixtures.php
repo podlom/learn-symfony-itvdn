1d637054be7be099c5a3655c3289a83202954977
Shkodenko V. Taras <taras.shkodenko@blackbit.de>
2022-07-29 16:33:06 +0300

    Improved UserFixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -14,10 +14,11 @@ class UserFixtures extends Fixture
     {
          $user = new User();
          $user->setEmail('podlom@gmail.com');
+         // @see: php bin/console security:hash-password
          // Hasher used     Symfony\Component\PasswordHasher\Hasher\MigratingPasswordHasher
          // ! [NOTE] Self-salting hasher used: the hasher generated its own built-in salt.
          $user->setPassword('$2y$13$ZMdsnksL4KVvkYLJsgXeX.GbR94S30o/TT4sxO3HlOPGI0nehVhnW'); // TestPass-123
-         $user->setRoles('ROLE_USER');
+         $user->setRoles(['ROLE_USER']);
 
          $manager->persist($user);
          $manager->flush();
