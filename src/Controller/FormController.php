1f675f5c5cfcd12a64d9f7e57f7a267d359a6045
Shkodenko V. Taras <taras.shkodenko@blackbit.de>
2022-07-30 18:55:44 +0300

    Improved FormController to redirect after adding a new product to a /product-added page

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,15 +19,19 @@ use App\Entity\Product;
 class FormController extends AbstractController
 {
     #[Route('/form', name: 'app_form')]
-    public function index(Request $request): Response
+    public function index(Request $request, ManagerRegistry $doctrine): Response
     {
         $post = new Product();
         $form = $this->createForm(PostType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $data = $form->getData();
-            dd($data);
+            $entityManager = $doctrine->getManager();
+            $product = $form->getData();
+            $entityManager->persist($product);
+            $entityManager->flush();
+
+            return new RedirectResponse('/product-added/' . $product->getId());
         }
 
         return $this->render('form/index.html.twig', [
