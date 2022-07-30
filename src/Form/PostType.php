f1a5ac8e606994ac8dac61b36b90954d72344a78
Shkodenko V. Taras <taras.shkodenko@blackbit.de>
2022-07-30 19:16:25 +0300

    Improved add product form PostType form validation rules

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -8,6 +8,9 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Validator\Constraints as Assert;
 
 
 class PostType extends AbstractType
@@ -15,11 +18,26 @@ class PostType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('price')
-            ->add('author')
-            ->add('sku')
+            ->add('name', TextType::class, [
+                'required' => true,
+                'constraints' => [new Assert\Length(['min' => 3, 'max' => 150])],
+            ])
+            ->add('description', TextareaType::class, [
+                'required' => true,
+                'constraints' => [new Assert\Length(['min' => 3])],
+            ])
+            ->add('price', TextType::class, [
+                'required' => true,
+                'constraints' => [new Assert\Positive()],
+            ])
+            ->add('author', TextType::class, [
+                'required' => false,
+                'constraints' => [new Assert\Length(['min' => 2, 'max' => 128])],
+            ])
+            ->add('sku', TextType::class, [
+                'required' => false,
+                'constraints' => [new Assert\Length(['min' => 3, 'max' => 128])],
+            ])
             ->add('save', SubmitType::class, ['label' => 'Save new Product'])
         ;
     }
