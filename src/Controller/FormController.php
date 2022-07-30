c874c5337d5c052934d8e46727f7077fe3933440
Shkodenko V. Taras <taras.shkodenko@blackbit.de>
2022-07-31 00:22:23 +0300

    Implemented /product-encoder/json and /product-encoder/xml product form data encoders

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -12,6 +12,10 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 use App\Form\PostType;
 use App\Entity\Product;
 
@@ -32,6 +36,34 @@ class FormController extends AbstractController
             $entityManager->flush();
 
             return new RedirectResponse('/product-added/' . $product->getId());
+        }
+
+        return $this->render('form/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/product-encoder/{format}', name: 'app_product_encoder')]
+    public function productEncoder(Request $request, string $format = 'json'): Response
+    {
+        $post = new Product();
+        $form = $this->createForm(PostType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $product = $form->getData();
+
+            $encoders = [new JsonEncoder(), new XmlEncoder()];
+            $normalizers = [new ObjectNormalizer()];
+            $serializer = new Serializer($normalizers, $encoders);
+
+            if ($format === 'json') {
+                $jsonContent = $serializer->serialize($product, 'json');
+                return new Response($jsonContent);
+            } else {
+                $xmlContent = $serializer->serialize($product, 'xml');
+                return new Response($xmlContent);
+            }
         }
 
         return $this->render('form/index.html.twig', [
