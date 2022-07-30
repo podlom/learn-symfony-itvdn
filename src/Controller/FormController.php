cecb0aa7797bc046cdd015d751583adf200bdbdc
Shkodenko V. Taras <taras.shkodenko@blackbit.de>
2022-07-30 18:43:23 +0300

    Geerated form PostType, Improved FormController to handle add product requests

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -4,23 +4,29 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use App\Form\PostType;
+use App\Entity\Product;
 
 
 class FormController extends AbstractController
 {
     #[Route('/form', name: 'app_form')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        $form = $this->createFormBuilder()
-            ->add('title', TextType::class)
-            ->add('description', TextareaType::class)
-            ->add('save', SubmitType::class, ['label' => 'Save Post'])
-            ->getForm();
+        $post = new Product();
+        $form = $this->createForm(PostType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            dd($data);
+        }
 
         return $this->render('form/index.html.twig', [
             'form' => $form->createView(),
---
+++ src/Form/PostType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+
+class PostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('price')
+            ->add('author')
+            ->add('sku')
+            ->add('save', SubmitType::class, ['label' => 'Save new Product'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
