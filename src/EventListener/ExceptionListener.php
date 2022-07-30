b9cc00dc030b04dec08ebc57b40f3b8ad6e41ec5
Shkodenko V. Taras <taras.shkodenko@blackbit.de>
2022-07-30 19:48:57 +0300

    Added src/EventListener/ExceptionListener and configured in config/services.yaml

---
+++ src/EventListener/ExceptionListener.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\EventListener;
+
+
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
+
+class ExceptionListener
+{
+    public function onKernelException(ExceptionEvent $event)
+    {
+        $exception = $event->getThrowable();
+        $message = 'Error: ' . $exception->getCode() . ' ' . $exception->getMessage();
+
+        $responce = new Response();
+        $responce->setContent($message);
+        $event->setResponse($responce);
+    }
+}
