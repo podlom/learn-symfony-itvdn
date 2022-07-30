f547ac240e5dc61913fdc6a2f5ee9b2093c4782f
Shkodenko V. Taras <taras.shkodenko@blackbit.de>
2022-07-31 01:28:54 +0300

    Extended tests/ProductTest.php

--- tests/ProductTest.php
+++ tests/ProductTest.php
@@ -3,7 +3,9 @@
 namespace App\Tests;
 
 
+use Doctrine\Persistence\ObjectManager;
 use PHPUnit\Framework\TestCase;
+use App\Entity\Product;
 
 
 class ProductTest extends TestCase
@@ -17,5 +19,21 @@ class ProductTest extends TestCase
         $c = $a + $b;
 
         $this->assertEquals(3, $c);
+    }
+
+    public function testAddProduct(): void
+    {
+        $entityManager = $this->createMock(ObjectManager::class);
+
+        $product = new Product();
+        $product->setName('Tested Product Name');
+        $product->setDescription('Tested Product Description');
+        $product->setPrice(75);
+        $product->setAuthor('Taras from PHPUnit');
+
+        $entityManager->persist($product);
+        $entityManager->flush();
+
+        $this->assertSame('Tested Product Name', $product->getName());
     }
 }
